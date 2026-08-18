c03027139e7564ee1ec43075c7c23ce9c333ae79
André Milani <andremilani34@gmail.com>
2026-08-18 20:19:37 -0300

    Fix: Ajustando path da logomarca

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ function App() {
     <main className="quiz-energisa">
       <img
         className="logomarca-energisa"
-        src="/EnergisaLogomarca.png"
+        src="./EnergisaLogomarca.png"
         alt="Logomarca Energisa"
       />
       <div className="palco-quiz">
